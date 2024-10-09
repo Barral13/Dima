9418b7397e5eec0176c2db54f9810085b7328334
Dhanglerson Barral <dhanglerson.dev@outlook.com>
2024-10-08 21:44:49 -0300

    Crud Transaction

---
+++ Dima.Core/Requests/Transactions/CreateTransactionRequest.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using Dima.Core.Enums;
+
+namespace Dima.Core.Requests.Transactions;
+
+public class CreateTransactionRequest : Request
+{
+    [Required(ErrorMessage = "Título inválido")]
+    [MaxLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
+    public string Title { get; set; } = string.Empty;
+
+
+    [Required(ErrorMessage = "Tipo inválido")]
+    public ETransactionType Type { get; set; }
+
+
+    [Required(ErrorMessage = "Valor inválido")]
+    public decimal Amount { get; set; }
+
+
+    [Required(ErrorMessage = "Categoria inválido")]
+    public long CategoryId { get; set; }
+
+
+    [Required(ErrorMessage = "Data inválido")]
+    public DateTime? PaidOrReceivedAt { get; set; }
+}
