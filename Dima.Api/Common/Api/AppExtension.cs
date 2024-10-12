883429b28dc1cf1ac5ada42ffe9c2671733f8c41
Dhanglerson Barral <dhanglerson.dev@outlook.com>
2024-10-11 22:00:05 -0300

    Configurations for Cors

---
+++ Dima.Api/Common/Api/AppExtension.cs
@@ -0,0 +1,21 @@
+namespace Dima.Api.Common.Api;
+
+public static class AppExtension
+{
+    public static void ConfigureDevEnvironment(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        });
+
+        app.MapSwagger().RequireAuthorization();
+    }
+
+    public static void UseSecurity(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
+}
